65fdf510ae82389da3a073940fd0dda5378d709a
Dhananjana94 <47080621+Dhananjana94@users.noreply.github.com>
2020-11-17 19:38:06 +0530

    some levels have created

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour {
+
+bool gameEnded = false;
+public GameObject LevelCompleteUI;
+
+public void LevelComplete(){
+
+	//Debug.Log("You Won Level");
+	LevelCompleteUI.SetActive(true);
+}
+
+//create game over function
+
+public void GameOver(){
+
+	if(gameEnded == false)
+	{
+		gameEnded = true;
+		Debug.Log("Game Over");
+		Invoke("Restart",2f);
+	}
+	
+}
+
+void Restart(){
+
+	SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+}
+
+}
